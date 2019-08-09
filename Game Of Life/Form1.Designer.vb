817810b81a9b1df7f5655fd75cd620a17d33884c
tankirules <35437320+tankirules@users.noreply.github.com>
2019-08-09 20:54:50 +1000

    V1.0 basic start, stop, reset and game

--- Game Of Life/Form1.Designer.vb
+++ Game Of Life/Form1.Designer.vb
@@ -22,18 +22,75 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnstartstop = New System.Windows.Forms.Button()
+        Me.lblx = New System.Windows.Forms.Label()
+        Me.lbly = New System.Windows.Forms.Label()
+        Me.btnreset = New System.Windows.Forms.Button()
+        Me.updatetimer = New System.Windows.Forms.Timer(Me.components)
         Me.SuspendLayout()
+        '
+        'btnstartstop
+        '
+        Me.btnstartstop.Location = New System.Drawing.Point(1202, 99)
+        Me.btnstartstop.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnstartstop.Name = "btnstartstop"
+        Me.btnstartstop.Size = New System.Drawing.Size(64, 33)
+        Me.btnstartstop.TabIndex = 0
+        Me.btnstartstop.Text = "Start"
+        Me.btnstartstop.UseVisualStyleBackColor = True
+        '
+        'lblx
+        '
+        Me.lblx.AutoSize = True
+        Me.lblx.Location = New System.Drawing.Point(1227, 200)
+        Me.lblx.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblx.Name = "lblx"
+        Me.lblx.Size = New System.Drawing.Size(0, 13)
+        Me.lblx.TabIndex = 1
+        '
+        'lbly
+        '
+        Me.lbly.AutoSize = True
+        Me.lbly.Location = New System.Drawing.Point(1227, 228)
+        Me.lbly.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lbly.Name = "lbly"
+        Me.lbly.Size = New System.Drawing.Size(0, 13)
+        Me.lbly.TabIndex = 2
+        '
+        'btnreset
+        '
+        Me.btnreset.Location = New System.Drawing.Point(1202, 171)
+        Me.btnreset.Name = "btnreset"
+        Me.btnreset.Size = New System.Drawing.Size(75, 23)
+        Me.btnreset.TabIndex = 3
+        Me.btnreset.Text = "Reset"
+        Me.btnreset.UseVisualStyleBackColor = True
+        '
+        'updatetimer
+        '
+        Me.updatetimer.Interval = 20
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1415, 647)
+        Me.Controls.Add(Me.btnreset)
+        Me.Controls.Add(Me.lbly)
+        Me.Controls.Add(Me.lblx)
+        Me.Controls.Add(Me.btnstartstop)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents btnstartstop As Button
+    Friend WithEvents lblx As Label
+    Friend WithEvents lbly As Label
+    Friend WithEvents btnreset As Button
+    Friend WithEvents updatetimer As Timer
 End Class
