b302a2d5c7c71b4643c8efd87a8b6dd5469f841e
tankirules <35437320+tankirules@users.noreply.github.com>
2019-08-09 17:47:12 +1000

    Added start/stop button, label to display which button is over

--- Game Of Life/Form1.Designer.vb
+++ Game Of Life/Form1.Designer.vb
@@ -22,18 +22,54 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.btnstartstop = New System.Windows.Forms.Button()
+        Me.lblx = New System.Windows.Forms.Label()
+        Me.lbly = New System.Windows.Forms.Label()
         Me.SuspendLayout()
+        '
+        'btnstartstop
+        '
+        Me.btnstartstop.Location = New System.Drawing.Point(1157, 123)
+        Me.btnstartstop.Name = "btnstartstop"
+        Me.btnstartstop.Size = New System.Drawing.Size(96, 51)
+        Me.btnstartstop.TabIndex = 0
+        Me.btnstartstop.Text = "Start"
+        Me.btnstartstop.UseVisualStyleBackColor = True
+        '
+        'lblx
+        '
+        Me.lblx.AutoSize = True
+        Me.lblx.Location = New System.Drawing.Point(1194, 278)
+        Me.lblx.Name = "lblx"
+        Me.lblx.Size = New System.Drawing.Size(0, 20)
+        Me.lblx.TabIndex = 1
+        '
+        'lbly
+        '
+        Me.lbly.AutoSize = True
+        Me.lbly.Location = New System.Drawing.Point(1194, 321)
+        Me.lbly.Name = "lbly"
+        Me.lbly.Size = New System.Drawing.Size(0, 20)
+        Me.lbly.TabIndex = 2
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1324, 692)
+        Me.Controls.Add(Me.lbly)
+        Me.Controls.Add(Me.lblx)
+        Me.Controls.Add(Me.btnstartstop)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents btnstartstop As Button
+    Friend WithEvents lblx As Label
+    Friend WithEvents lbly As Label
 End Class
--- Game Of Life/Form1.vb
+++ Game Of Life/Form1.vb
@@ -2,7 +2,10 @@
     Dim Grid(50, 50) As Button
     Dim Cells(50, 50) As Integer
     Dim SideLength As Integer
+    Dim TempGrid(50, 50) As Integer
+    Dim Started As Boolean
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Started = False
         SideLength = Me.Bounds.Height / 60
         For x = 1 To 50
             For y = 1 To 50
@@ -17,5 +20,22 @@
             Next
         Next
 
+    End Sub
+    Private Sub Grid_Select(sender As Object, e As EventArgs)
+        Dim xpos, ypos As Integer
+        xpos = CInt(sender.location.x) / SideLength
+        ypos = CInt(sender.location.y) / SideLength
+        lblx.Text = "X Pos: " + CStr(xpos)
+        lbly.Text = "Y Pos: " + CStr(ypos)
+    End Sub
+
+    Private Sub btnstartstop_Click(sender As Object, e As EventArgs) Handles btnstartstop.Click
+        If Started = False Then
+            btnstartstop.Text = "Stop"
+            Started = True
+        ElseIf Started = True Then
+            btnstartstop.Text = "Start"
+            Started = False
+        End If
     End Sub
 End Class
