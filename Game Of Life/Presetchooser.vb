cfeb39c62e1006e83f8e33bd9bd371740cf2277c
tankirules <tankirulesipad1@gmail.com>
2020-02-28 23:02:24 +1100

    Lots of loading code to load file

--- Game Of Life/Presetchooser.vb
+++ Game Of Life/Presetchooser.vb
@@ -9,10 +9,14 @@
     Dim preset3(50, 50) As Integer
     Dim preset4(50, 50) As Integer
     Dim preset5(50, 50) As Integer
+
     Dim cpreset1, cpreset2, cpreset3, cpreset4, cpreset5, cpreset6 As Boolean
     Dim centerbeingset As Boolean
 
     Private Sub Presetchooser_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim arrayofpresests(6) As Integer
+        Dim presetcountload As Integer
+        presetcountload = 0
         centerbeingset = False
         cpreset1 = False
         cpreset2 = False
@@ -21,6 +25,149 @@
         cpreset5 = False
         cpreset6 = False
         Dim loadedfile() As String = IO.File.ReadAllLines("C:\Gameoflife\presets.txt")
+        Dim presetlinelist As New List(Of Integer)
+        Dim templist As New List(Of String)
+        For i = 0 To loadedfile.Length - 1
+            Console.WriteLine("Loaded file line: " + CStr(i) + " is " + loadedfile(i))
+        Next
+
+        If loadedfile(0).Contains("P1") Then
+            presetlinelist.Add(0)
+            presetcountload += 1
+            Console.WriteLine("P1 exists!")
+            For i = 1 To (loadedfile.Length() - 1)
+
+                Console.WriteLine(loadedfile.Length())
+                If loadedfile(i).Contains("P2") Then
+                    If presetcountload = 1 Then
+                        presetlinelist.Add(i)
+                        presetcountload += 1
+                        Console.WriteLine("P2 exists!")
+                    Else
+                        MsgBox("Preset lines in file corrupt! Recommend deleting C:\Gameoflife\presets.txt and restarting program!")
+                        Console.WriteLine("Corrupt 2")
+                        Exit For
+                    End If
+                End If
+                If loadedfile(i).Contains("P3") Then
+                    If presetcountload = 2 Then
+                        presetlinelist.Add(i)
+                        presetcountload += 1
+                        Console.WriteLine("P3 exists!")
+                    Else
+                        MsgBox("Preset lines in file corrupt! Recommend deleting C:\Gameoflife\presets.txt and restarting program!")
+                        Console.WriteLine("Corrupt 3")
+                        Exit For
+                    End If
+                End If
+                If loadedfile(i).Contains("P4") Then
+                    If presetcountload = 3 Then
+                        presetlinelist.Add(i)
+                        presetcountload += 1
+                        Console.WriteLine("P4 exists!")
+                    Else
+                        MsgBox("Preset lines in file corrupt! Recommend deleting C:\Gameoflife\presets.txt and restarting program!")
+                        Console.WriteLine("Corrupt 4")
+                        Exit For
+                    End If
+                End If
+                If loadedfile(i).Contains("P5") Then
+                    If presetcountload = 4 Then
+                        presetlinelist.Add(i)
+                        presetcountload += 1
+                        Console.WriteLine("P5 exists!")
+                    Else
+                        MsgBox("Preset lines in file corrupt! Recommend deleting C:\Gameoflife\presets.txt and restarting program!")
+                        Console.WriteLine("Corrupt 5")
+                        Exit For
+                    End If
+                End If
+                If loadedfile(i).Contains("P6") Then
+                    If presetcountload = 5 Then
+                        presetlinelist.Add(i)
+                        presetcountload += 1
+                        Console.WriteLine("P6 exists!")
+                    Else
+                        MsgBox("Preset lines in file corrupt! Recommend deleting C:\Gameoflife\presets.txt and restarting program!")
+                        Console.WriteLine("Corrupt 6")
+                        Exit For
+                    End If
+                End If
+            Next
+            If presetcountload = 6 Then
+                Console.WriteLine("Loading successful! Loaded 6 presets")
+            Else
+                MsgBox("Preset lines in file corrupt! Recommend deleting C:\Gameoflife\presets.txt and restarting program!")
+                Console.WriteLine(CStr(presetcountload))
+                Console.WriteLine("at the end presetcountload wasnt 6")
+            End If
+        Else
+            MsgBox("Preset lines in file corrupt! Recommend deleting C:\Gameoflife\presets.txt and restarting program!")
+            Console.WriteLine("Corrupt because first line isn't P1")
+            'if first line isn't P1
+        End If
+
+
+
+        For i = 0 To presetlinelist(0) - 1
+            templist.Add(loadedfile(i))
+        Next
+        Dim ptemp1 As Object = templist.ToArray
+        For i = 0 To ptemp1.length - 1
+            ptemp1(i) = CStr(ptemp1(i))
+        Next
+        'put everything related to preset1 into array
+        templist.Clear()
+
+        For i = presetlinelist(1) To presetlinelist(2) - 1
+            templist.Add(loadedfile(i))
+        Next
+        Dim ptemp2 As Object = templist.ToArray
+        For i = 0 To ptemp2.length - 1
+            ptemp2(i) = CStr(ptemp2(i))
+        Next
+        'put everything related to preset2 into array
+        templist.Clear()
+
+        For i = presetlinelist(2) To presetlinelist(3) - 1
+            templist.Add(loadedfile(i))
+        Next
+        Dim ptemp3 As Object = templist.ToArray
+        For i = 0 To ptemp3.length - 1
+            ptemp3(i) = CStr(ptemp3(i))
+        Next
+        'put everything related to preset3 into array
+        templist.Clear()
+
+        For i = presetlinelist(3) To presetlinelist(4) - 1
+            templist.Add(loadedfile(i))
+        Next
+        Dim ptemp4 As Object = templist.ToArray
+        For i = 0 To ptemp4.length - 1
+            ptemp4(i) = CStr(ptemp4(i))
+        Next
+        'put everything related topreset 4 into array
+        templist.Clear()
+        For i = presetlinelist(4) To presetlinelist(5) - 1
+            templist.Add(loadedfile(i))
+        Next
+        Dim ptemp5 As Object = templist.ToArray
+        For i = 0 To ptemp5.length - 1
+            ptemp5(i) = CStr(ptemp5(i))
+        Next
+        templist.Clear()
+        'put everything related to preset 5 into array
+        For i = presetlinelist(5) To loadedfile.Length() - 1
+            templist.Add(loadedfile(i))
+        Next
+        Dim ptemp6 As Object = templist.ToArray
+        For i = 0 To ptemp6.length - 1
+            ptemp6(i) = CStr(ptemp6(i))
+        Next
+
+
+
+
 
 
         closedproperlyinput = False
