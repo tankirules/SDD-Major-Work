7b1d4818bbd90df4b97ca6a5e6ce1912190f6193
school computer <35437320+tankirules@users.noreply.github.com>
2020-03-17 12:16:58 +1100

    loaded usernames this session

--- Game Of Life/Form1.Designer.vb
+++ Game Of Life/Form1.Designer.vb
@@ -181,11 +181,11 @@ Partial Class Form1
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(1153, 9)
+        Me.Label2.Location = New System.Drawing.Point(1095, 9)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(248, 25)
+        Me.Label2.Size = New System.Drawing.Size(316, 25)
         Me.Label2.TabIndex = 19
-        Me.Label2.Text = "Past Loaded Usernames"
+        Me.Label2.Text = "Loaded usernames this session"
         '
         'btncancelpresetplacement
         '
